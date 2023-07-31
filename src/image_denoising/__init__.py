66b75f73f7024588a4bfecd3559b877d05e1ee4d
Vicente González-Ruiz <vruiz@ual.es>
2023-07-31 09:58:29 +0200

    .

--- src/image_denoising/__init__.py
+++ src/image_denoising/__init__.py
@@ -3,9 +3,13 @@ logger = logging.getLogger(__name__)
 #logging.basicConfig(format="[%(filename)s:%(lineno)s %(funcName)s()] %(message)s")
 #logger.setLevel(logging.CRITICAL)
 #logger.setLevel(logging.ERROR)
-logger.setLevel(logging.WARNING)
-#logger.setLevel(logging.INFO)
+#logger.setLevel(logging.WARNING)
+logger.setLevel(logging.INFO)
 #logger.setLevel(logging.DEBUG)
 
 logger.info("__init__ run")
 print("__init__ run")
+
+import gaussian
+import OF_gaussian
+import random
