52a79d48acb8e3fff627d1df0eda063aeeab5b0f
Vicente González-Ruiz <vruiz@ual.es>
2023-09-03 20:11:39 +0200

    .

--- src/image_denoising/OF_random.py
+++ src/image_denoising/OF_random.py
@@ -13,8 +13,8 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(format="[%(filename)s:%(lineno)s %(funcName)s()] %(message)s")
 #logger.setLevel(logging.CRITICAL)
 #logger.setLevel(logging.ERROR)
-#logger.setLevel(logging.WARNING)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.WARNING)
+#logger.setLevel(logging.INFO)
 #logger.setLevel(logging.DEBUG)
 
 if logger.getEffectiveLevel() < logging.WARNING:
