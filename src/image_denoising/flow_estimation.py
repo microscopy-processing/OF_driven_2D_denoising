72589ef1e20cac39d4b1ce1a5c383ad698be317c
Vicente González-Ruiz <vruiz@ual.es>
2023-09-27 05:26:14 +0200

    .

--- src/image_denoising/flow_estimation.py
+++ src/image_denoising/flow_estimation.py
@@ -1,17 +1,10 @@
 import numpy as np
 import cv2
-import image_denoising
+#import image_denoising
 #from motion_estimation import farneback
 #from motion_estimation import predict
-import motion_estimation
 import logging
-import image_denoising
-
-#image_denoising.logger.setLevel(logging.CRITICAL)
-#image_denoising.logger.setLevel(logging.ERROR)
-#image_denoising.logger.setLevel(logging.WARNING)
-#image_denoising.logger.setLevel(logging.INFO)
-#image_denoising.logger.setLevel(logging.DEBUG)
+#import image_denoising
 
 import logging
 logger = logging.getLogger(__name__)
@@ -24,6 +17,55 @@ logger.setLevel(logging.WARNING)
 
 #image_denoising.logger.info(f"Logging level: {image_denoising.logger.getEffectiveLevel()}")
 
+#pip install "color_transforms @ git+https://github.com/vicente-gonzalez-ruiz/motion_estimation"
+import motion_estimation
+
+class Farneback_Flow_Estimator(motion_estimation.frarneback.Estimator_in_CPU):
+
+    def __init__(self,
+                 levels=3,
+                 pyr_scale=0.5,
+                 fast_piramids=False,
+                 win_side=15,
+                 iters=5,
+                 poly_n=5,
+                 pyr_sigma=1.5,
+                 flags=cv2.OPTFLOW_FARNEBACK_GAUSSIAN):
+        
+        super().__init__(levels,
+                         pyr_scale,
+                         fast_piramids,
+                         win_side,
+                         iters,
+                         poly_n=,
+                         pyr_sigma,
+                         flags)
+    
+    def get_flow_to_project_A_to_B(A, B):
+        flow = self.get_flow(target=A, reference=B, prev_flow=None)
+        return flow
+    
+def project(image, flow):
+    warped_image = motion_estimation.predict.warp(
+        reference=image,
+        flow=flow,
+        interpolation_mode=cv2.INTER_LINEAR,
+        extension_mode=cv2.BORDER_REPLICATE)
+    #height, width = flow.shape[:2]
+    #map_x = np.tile(np.arange(width), (height, 1))
+    #map_y = np.swapaxes(np.tile(np.arange(height), (width, 1)), 0, 1)
+    #map_xy = (flow + np.dstack((map_x, map_y))).astype('float32')
+    #warped_image = cv2.remap(image, map_xy, None,
+    #                         #interpolation=cv2.INTER_LANCZOS4,
+    #                         interpolation=cv2.INTER_LINEAR,
+    #                         #interpolation=cv2.INTER_CUBIC,
+    #                         #interpolation=cv2.INTER_NEAREST,
+    #                         #interpolation=cv2.INTER_AREA,
+    #                         borderMode=cv2.BORDER_REPLICATE)
+    return warped_image
+
+
+'''
 def get_flow_to_project_A_to_B(A, B, l=3, w=15, prev_flow=None, sigma=1.5, iterations=5):
     # projection(next, flow) ~ prev
     flow = motion_estimation.farneback.get_flow(
@@ -48,22 +90,5 @@ def get_flow_to_project_A_to_B(A, B, l=3, w=15, prev_flow=None, sigma=1.5, itera
 
     #print(f"avg_OF={np.average(np.abs(flow))}, l={l}, w={w}, sigma={sigma}", end=' ')
     return flow
+'''
 
-def project(image, flow):
-    warped_image = motion_estimation.predict.warp(
-        reference=image,
-        flow=flow,
-        interpolation_mode=cv2.INTER_LINEAR,
-        extension_mode=cv2.BORDER_REPLICATE)
-    #height, width = flow.shape[:2]
-    #map_x = np.tile(np.arange(width), (height, 1))
-    #map_y = np.swapaxes(np.tile(np.arange(height), (width, 1)), 0, 1)
-    #map_xy = (flow + np.dstack((map_x, map_y))).astype('float32')
-    #warped_image = cv2.remap(image, map_xy, None,
-    #                         #interpolation=cv2.INTER_LANCZOS4,
-    #                         interpolation=cv2.INTER_LINEAR,
-    #                         #interpolation=cv2.INTER_CUBIC,
-    #                         #interpolation=cv2.INTER_NEAREST,
-    #                         #interpolation=cv2.INTER_AREA,
-    #                         borderMode=cv2.BORDER_REPLICATE)
-    return warped_image
