e940454c522c5b025bf8c54d1dd1a08517982ab1
Vicente González-Ruiz <vruiz@ual.es>
2024-03-06 07:53:07 +0100

    .

--- src/image_denoising/OF_gaussian.py
+++ src/image_denoising/OF_gaussian.py
@@ -107,7 +107,7 @@ def __gray_OF_gaussian_filtering(noisy_image, kernel, l=3, w=5, sigma=0.5):
     filtered_noisy_image = (filtered_noisy_image_in_vertical + filtered_noisy_image_in_horizontal)/2
     return filtered_noisy_image
 
-def gray_OF_gaussian_filtering(self, noisy_img, kernel, l=3, w=5, sigma=0.5):
+def gray_OF_gaussian_filtering(noisy_img, kernel, l=3, w=5, sigma=0.5):
     filtered_noisy_img_Y = gray_vertical_OF_gaussian_filtering(noisy_img, kernel, l, w, sigma)
     transposed_filtered_noisy_img_Y = np.transpose(filtered_noisy_img_Y, (1, 0))
     transposed_filtered_noisy_img_YX = gray_vertical_OF_gaussian_filtering(transposed_filtered_noisy_img_Y, kernel, l, w, sigma)
